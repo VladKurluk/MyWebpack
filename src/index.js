7ef8cfe135087c51301d6dfd744a5c8131a2bb1b
Vladislav Kurluk <vlad11kurluk89@gmail.com>
2020-05-26 15:31:57 +0300

    Старт. Базовая настройка Webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Post from './Post'
+import './styles/styles.css'
+
+const post = new Post('Webpack Start')
+console.log('Post to string', post.toString())
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+// Встроенные плагины
+const path = require('path')
+// Установленные плагины
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin')
+
+module.exports = {
+  // Режим работы
+  mode: 'development',
+  // В поле context храниться абсолютный путь к папке с исходниками проекта
+  context: path.resolve(__dirname, 'src'),
+  // Входные точки
+  entry: {
+    main: './index.js',
+    analytics: './analytics.js'
+  },
+  // Результат работы сборщика
+  // В [name] попадет ключи из объекта entry. Ф-лов будет ровно столько сколько ключей.
+  // В [contenthash] уникальный хеш, который измениться при изменении содержимого ф-ла
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
+  }
+}
